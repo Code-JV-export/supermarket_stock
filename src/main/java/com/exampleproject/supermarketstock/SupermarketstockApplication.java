209b736d3b0282d642d843b81f9bbb02ed9df6d2
Code-JV-export <josevagner9@gmail.com>
2021-08-02 20:03:08 -0300

    created the project and, created funcionalit the API

---
+++ src/main/java/com/exampleproject/supermarketstock/SupermarketstockApplication.java
@@ -0,0 +1,13 @@
+package com.exampleproject.supermarketstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exampleproject/supermarketstock/SupermarketstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleproject.supermarketstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
